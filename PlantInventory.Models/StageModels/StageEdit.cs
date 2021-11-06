99252835e8a1c03e07bf46a3d56fe84c2815b07f
David Behrmann <davidbehrmanntx@gmail.com>
2021-11-06 11:14:39 -0400

    finished service layer MVP

--- PlantInventory.Models/StageModels/StageEdit.cs
+++ PlantInventory.Models/StageModels/StageEdit.cs
@@ -1,27 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PlantInventory.Data
+namespace PlantInventory.Models.StageModels
 {
-    public class Stage
+    public class StageEdit
     {
-        [Key]
         public int StageId { get; set; }
-        [Required]
-        public Guid UserId { get; set; }
-        [Required]
-        [ForeignKey("Batch")]
         public int BatchId { get; set; }
-        public virtual Batch Batch { get; set; }
         public int CountGrowRoom { get; set; }
         public int CountPacking { get; set; }
         public int CountFreshCut { get; set; }
         public int CountDump { get; set; }
-
     }
 }
---
+++ PlantInventory.Services/StageService.cs
@@ -0,0 +1,70 @@
+﻿using PlantInventory.Data;
+using PlantInventory.Models.StageModels;
+using PlantInventory.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantInventory.Services
+{
+    public class StageService
+    {
+        private readonly Guid _userID;
+        public StageService(Guid userID)
+        {
+            _userID = userID;
+        }
+        //Create Stage
+        public bool CreateBatch(StageCreate model)
+        {
+            var entity = new Stage()
+            {
+                UserId = _userID,
+                BatchId = model.BatchId,
+                CountGrowRoom = model.CountGrowRoom,
+                CountPacking = model.CountPacking,
+                CountFreshCut = model.CountFreshCut,
+                CountDump = model.CountDump
+            };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Stages.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        //Get Stage by ID (stage and batch are 1 to 1)
+        public StageCreate GetStageByID(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Stages.Single(e => e.BatchId == id);
+                return new StageCreate
+                {
+                    BatchId = entity.BatchId,
+                    CountGrowRoom = entity.CountGrowRoom,
+                    CountPacking = entity.CountPacking,
+                    CountFreshCut = entity.CountFreshCut,
+                    CountDump = entity.CountDump
+                };
+            }
+        }
+
+        //Update Stage 
+        public bool UpdateStage(StageEdit model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Stages.Single(e => e.StageId == model.StageId);
+
+                entity.CountGrowRoom = model.CountGrowRoom;
+                entity.CountPacking = model.CountPacking;
+                entity.CountFreshCut = model.CountFreshCut;
+                entity.CountDump = model.CountDump;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
