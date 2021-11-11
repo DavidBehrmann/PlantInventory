a585c31758b3bc71616209c586600450d1f350b0
David Behrmann <davidbehrmanntx@gmail.com>
2021-11-10 21:13:23 -0500

    finished MVP MoveController

--- PlantInventory.MVC/Controllers/MoveController.cs
+++ PlantInventory.MVC/Controllers/MoveController.cs
@@ -63,5 +63,64 @@ namespace PlantInventory.MVC.Controllers
             ModelState.AddModelError("", "This move has NOT been recorded. Please ensure your data is correct.");
             return View(model);
         }
+        public ActionResult GetArchive(int batchId)
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new MoveService(userId);
+            var model = service.GetAllMovesForABatchArchived(batchId);
+
+            return View(model);
+        }
+
+        public ActionResult GetMoveByID(int id)
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new MoveService(userId);
+            var model = service.GetMoveByID(id);
+
+            return View(model);
+        }
+
+        public ActionResult EditMove(int id)
+        {
+            var service = CreateMoveService();
+            var edit = service.GetMoveByID(id);
+            var model = new MoveEdit
+            {
+                MoveFrom = edit.MoveFrom,
+                MoveTo = edit.MoveTo,
+                NumberOfPotsMoved = edit.NumberOfPotsMoved,
+                Comment = edit.Comment,
+                ModifiedUTC = DateTimeOffset.Now,
+                IsArchived = edit.IsArchived,
+                ArchiveComment = edit.ArchiveComment
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditMove(int id, MoveEdit model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            if (model.MoveId != id)
+            {
+                ModelState.AddModelError("", "ID Mismatch");
+                return View(model);
+            }
+
+            var service = CreateMoveService();
+
+            if (service.EditMove(model))
+            {
+                TempData["SaveResult"] = "You have updated the move.";
+                return RedirectToAction("Index"); //could give me problems without a specified ID for the index
+            }
+
+            ModelState.AddModelError("", "We were unable to update the move. Please ensure your data is correct.");
+            return View(model);
+        }   
+        
     }
 }
